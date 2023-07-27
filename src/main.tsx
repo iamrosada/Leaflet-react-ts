4340e2f312c729a8e9ba1d9877dead51d89caab7
iamrosada <luisrosada2018@gmail.com>
2023-07-28 02:43:38 +0300

    feat: setup of Provider from redux to use the store to all application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App";
+import "@/styles/main.scss";
+import { Provider } from "react-redux";
+import store from "@/store";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
+  </React.StrictMode>
+);
