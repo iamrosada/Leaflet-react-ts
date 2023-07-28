0dc1bb8afc6ccc7c4ab64987b6a61089bb46139e
iamrosada <luisrosada2018@gmail.com>
2023-07-28 12:55:34 +0300

    feat: created the function to request the Polyline by coordinates

--- src/services/routeService.ts
+++ src/services/routeService.ts
@@ -1,16 +1,28 @@
 import axios from "axios";
+import polyline from "@mapbox/polyline";
 
-const API_BASE_URL = "http://project-osrm.org/osrm";
+const API_BASE_URL = "http://router.project-osrm.org";
 
 export async function getPolylineFromAPI(markers: any) {
+  const profile = "driving";
   const coordinates = markers
-    .map((marker: any[]) => `${marker[1]},${marker[0]}`)
+    .map((marker: any) => `${marker[1]},${marker[0]}`)
     .join(";");
-  const response = await axios.get(
-    `${API_BASE_URL}/route/v1/driving/${coordinates}`
-  );
-  return response.data.routes[0].geometry.coordinates.map((coord: any[]) => [
-    coord[1],
-    coord[0],
-  ]);
+  const url = `${API_BASE_URL}/route/v1/${profile}/${coordinates}`;
+
+  try {
+    const response = await axios.get(url);
+
+    if (response.status === 200 && response.data.code === "Ok") {
+      const polylineString = response.data.routes[0].geometry;
+      const decodedCoordinates = polyline.decode(polylineString);
+      return decodedCoordinates.map((coord) => [coord[1], coord[0]]);
+    } else {
+      console.error("Error fetching route data:", response.data);
+      return [];
+    }
+  } catch (error) {
+    console.error("Error fetching route data:", error);
+    return [];
+  }
 }
