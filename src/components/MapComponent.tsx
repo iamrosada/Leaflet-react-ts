cc27dd13d36da4df81487045e9bb166a8405845e
iamrosada <luisrosada2018@gmail.com>
2023-07-28 15:15:36 +0300

    feat: added bounds to show the local exactly of coordinates

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,12 +1,26 @@
 import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
+import MapContent from "@/components/MapContent";
 
 const MapComponent = ({ selectedRoute }: any) => {
   const center = selectedRoute?.polyline?.length
     ? selectedRoute.polyline[0]
     : [59.84660399, 30.29496392];
-  console.log("selectedRoute.polyline[0]", selectedRoute);
+
+  const bounds =
+    selectedRoute?.polyline?.length > 0
+      ? selectedRoute.polyline.map((point: number[]) => [point[0], point[1]])
+      : [
+          [59.84660399, 30.29496392],
+          [59.84660399, 30.29496392],
+        ];
+
   return (
-    <MapContainer center={center} zoom={13}>
+    <MapContainer
+      center={center}
+      zoom={13}
+      style={{ height: "400px", width: "600px" }}
+    >
+      <MapContent bounds={bounds} />
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {selectedRoute?.polyline && (
         <Polyline positions={selectedRoute.polyline} color="red" />
