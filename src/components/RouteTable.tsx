ff5630acdbf3103c49aebbcf0c8cdeaa1b456b31
iamrosada <luisrosada2018@gmail.com>
2023-07-28 13:52:13 +0300

    feat: created component to display the Table with cars

---
+++ src/components/RouteTable.tsx
@@ -0,0 +1,33 @@
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "Маршрут",
+    dataIndex: "name",
+    key: "name",
+  },
+];
+
+const RouteTable = ({ routes, selectedRouteId, onRowClick }: any) => {
+  const data = routes.map((route: any, index: any) => ({
+    key: index,
+    name: `Маршрут №${index + 1}`,
+  }));
+
+  console.log(data);
+  return (
+    <Table
+      className="sidebar"
+      dataSource={data}
+      columns={columns}
+      onRow={(record, rowIndex) => {
+        return {
+          onClick: () => onRowClick(rowIndex),
+          className: rowIndex === selectedRouteId ? "selected-row" : "",
+        };
+      }}
+    />
+  );
+};
+
+export default RouteTable;
