5666dd66215ff25cf443c11474e6cf17b5889071
iamrosada <luisrosada2018@gmail.com>
2023-07-28 13:43:38 +0300

    fix: removed the function from library to encode the polyline and was created the own function to converte string poyline to real coordinates markes

--- src/services/routeService.ts
+++ src/services/routeService.ts
@@ -1,22 +1,29 @@
 import axios from "axios";
-import polyline from "@mapbox/polyline";
+import decodePolyline from "@/utilities/decodePolyline";
 
 const API_BASE_URL = "http://router.project-osrm.org";
 
-export async function getPolylineFromAPI(markers: any) {
+export async function getPolylineFromAPI(markers: number[][]) {
   const profile = "driving";
+  console.log(markers, "markers");
   const coordinates = markers
     .map((marker: any) => `${marker[1]},${marker[0]}`)
     .join(";");
+
   const url = `${API_BASE_URL}/route/v1/${profile}/${coordinates}`;
 
   try {
     const response = await axios.get(url);
 
     if (response.status === 200 && response.data.code === "Ok") {
-      const polylineString = response.data.routes[0].geometry;
-      const decodedCoordinates = polyline.decode(polylineString);
-      return decodedCoordinates.map((coord) => [coord[1], coord[0]]);
+      const encoded = response.data.routes[0].geometry;
+
+      const decodedCoordinates = decodePolyline(encoded);
+
+      return decodedCoordinates.map((coord) => [
+        coord.latitude,
+        coord.longitude,
+      ]);
     } else {
       console.error("Error fetching route data:", response.data);
       return [];
